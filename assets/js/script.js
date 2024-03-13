1d000065cf588c0bf3ec092c89c346eda782d0ac
bmallar <benmallar@gmail.com>
2024-03-13 13:53:56 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,5 @@
 // Update questions every 24 hours, (I think this would go at the bottom of the apiRequest function)
-setInterval(apiRequest, 24 * 60 * 60 * 1000); // 24 hours in milliseconds, (here for later use)
+setInterval(apiRequest, 24 * 60 * 60 * 1000); // 24 hours in milliseconds, (here for later use) 
+// We have to locally store the questions. 
+// When the user accesses the page check if they are stored in the local storage.
+//If they are, dislplay those questions; if not, fetch the questions from the server.
