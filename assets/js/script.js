8b0e22242693331f0f022e7c2bcd52b777a4a20c
bmallar <benmallar@gmail.com>
2024-03-13 13:35:36 -0500

    getting rid of the space I added on accident

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-// Update questions every 24 hours
-setInterval(updateQuestions, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+// Update questions every 24 hours, (I think this would go at the bottom of the apiRequest function)
+setInterval(apiRequest, 24 * 60 * 60 * 1000); // 24 hours in milliseconds, (here for later use)
